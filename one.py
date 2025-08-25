3fbbc0b23e94a17e92d9a776683e845e68464f2c
Pradeep <grpspradeep@gmail.com>
2025-08-25 16:36:22 +0530

    Inital Commit

---
+++ one.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("📊 Price Calculator")
+
+# Take inputs from user
+lp = st.number_input("Enter Landing Price (25mm): ", min_value=0.0, step=0.1)
+ds = st.number_input("Enter Desired Size: ", min_value=0.0, step=0.1)
+
+# Calculate
+if cost_price > 0:
+    lp = lp * (0.79)
+    lp = (lp * (1.16))*(1.115)
+    lpf = lp * (ds/25)
+    st.write(f"✅ Final Price (25mm): {lp}")
+    st.write(f"📈 Final Price {ds}mm: {lpf}")
+else:
+    st.warning("Enter valid values!")
